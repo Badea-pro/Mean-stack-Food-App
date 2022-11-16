a78b4aeadaa56ddb642b4635820dac4a790b4dc6
Badea-pro <79918603+Badea-pro@users.noreply.github.com>
2022-11-16 11:31:07 +0300

    Badea is the best

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compononets/partials/header/header.component';
+import { HomeComponent } from './compononets/pages/home/home.component';
+import { RatingModule } from 'ng-starrating';
+import { SearchComponent } from './compononets/partials/search/search.component';
+import { TagsComponent } from './compononets/partials/tags/tags.component';
+import { FoodPageComponent } from './compononets/pages/food-page/food-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    FoodPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RatingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
